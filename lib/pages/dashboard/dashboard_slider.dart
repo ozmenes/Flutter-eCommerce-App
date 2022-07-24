fbdb3f2072aaa0e67cca909c10d869035621e12a
EnesOZMEN <ozmen.enes@hotmail.com>
2022-07-24 23:55:40 +0200

    Initial commit

--- lib/pages/dashboard/dashboard_slider.dart
+++ lib/pages/dashboard/dashboard_slider.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class DashboardSlider extends StatefulWidget {
@@ -10,11 +11,28 @@ class DashboardSlider extends StatefulWidget {
 class _DashboardSliderState extends State<DashboardSlider> {
   @override
   Widget build(BuildContext context) {
-    final _size = MediaQuery.of(context).size;
-    return Container(
-      height: 400,
-      width: _size.width,
-      color: Colors.blue,
+    //final _size = MediaQuery.of(context).size;
+    return CarouselSlider(
+      options: CarouselOptions(height: 320.0,),
+      items: [1,2,3,4,5].map((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                width: MediaQuery.of(context).size.width*2,
+                margin: const EdgeInsets.symmetric(horizontal: 1.0),
+                decoration: const BoxDecoration(
+                    color: Colors.greenAccent
+                ),
+                child: Text('text $i', style: const TextStyle(fontSize: 16.0),)
+            );
+          },
+        );
+      }).toList(),
     );
+    // return Container(
+    //   height: 400,
+    //   width: _size.width,
+    //   color: Colors.blue,
+    // );
   }
 }
