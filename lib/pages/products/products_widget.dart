b5669ee0f537eb5c1b79c68e08e87aac05c89763
EnesOZMEN <ozmen.enes@hotmail.com>
2022-07-27 22:05:01 +0200

    Initial commit

---
+++ lib/pages/products/products_widget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ProductsWidget extends StatefulWidget {
+  const ProductsWidget({Key? key}) : super(key: key);
+
+  @override
+  _ProductsWidgetState createState() => _ProductsWidgetState();
+}
+
+class _ProductsWidgetState extends State<ProductsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Center(
+        child: Text('Products'),
+      ),
+    );
+  }
+}
