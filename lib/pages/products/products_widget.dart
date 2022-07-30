e146eb42634045f8f8bec2eb8dc684cf8b756cae
EnesOZMEN <ozmen.enes@hotmail.com>
2022-07-30 14:34:34 +0200

    Initial commit

--- lib/pages/products/products_widget.dart
+++ lib/pages/products/products_widget.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_flutter_app/pages/cart/cart_page.dart';
 import 'package:flutter/material.dart';
 
 class ProductsWidget extends StatefulWidget {
@@ -11,11 +12,31 @@ class _ProductsWidgetState extends State<ProductsWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: const Center(
-          child: Text('Products Details'),
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+                height: 300,
+                child: Image(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                      "https://i.morioh.com/2021/07/28/6b7ee0a2.webp"),
+                )),
+            const Text("Quantity: 1"),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (builder) => const CartPage()));
+              },
+              child: const Text('Add to Cart'),
+            ),
+            const Text("Product details"),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
